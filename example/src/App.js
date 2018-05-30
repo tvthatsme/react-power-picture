dc9db35e8e9ca2b8e8e217d3caddd96fd38e301c
Timothy Vernon <timothy.n.vernon@gmail.com>
2018-05-30 16:59:11 +0400

    Add core logic to component

--- example/src/App.js
+++ example/src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
-
 import PowerPicture from 'react-power-picture';
 
 export default class App extends Component {
   render() {
+    const sources = [
+      {
+        size: 400,
+        src: 'https://source.unsplash.com/random/200x140'
+      },
+      {
+        size: 800,
+        src: 'https://source.unsplash.com/random/300x200'
+      },
+      {
+        size: 1200,
+        src: 'https://source.unsplash.com/random/400x300'
+      }
+    ];
     return (
       <div>
         <h1 className="header">React Power Picture Example</h1>
         <div className="center">
-          <PowerPicture />
+          <PowerPicture sources={sources}>
+            {(image, loading) => (
+              <div className="text-center">
+                <p>Loading state: {loading.toString()}</p>
+                <img alt="A p!cture is worth a thousand words" src={image} />
+              </div>
+            )}
+          </PowerPicture>
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,118 @@
 import React, { Component } from 'react';
 
-export default class PowerPicture extends Component {
+class PowerPicture extends Component {
+  constructor(props) {
+    super(props);
+
+    // define an internal variable for the image that will be loaded
+    this.image = null;
+
+    // define the component state
+    this.state = {
+      image: null,
+      loading: true
+    };
+  }
+
+  /**
+   * Start loading the right image once the component mounts
+   */
+  componentDidMount() {
+    const { sources } = this.props;
+    const imgToLoad = this.getIdealSize(sources, window.innerWidth);
+    this.loadImage(imgToLoad);
+  }
+
+  /**
+   * Unsubscribe any events that the image loading might have subscribed to
+   */
+  componentWillUnmount() {
+    if (this.image) {
+      this.image.onload = null;
+      this.image.onerror = null;
+    }
+  }
+
+  /**
+   * Determine whether the image needs to be changed after a component update
+   *
+   * @param {Object} prevProps props of component before update
+   * @param {Object} prevState state of component before update
+   */
+  componentDidUpdate(prevProps, prevState) {
+    const { sources, placeholder } = this.props;
+    const imgToLoad = this.getIdealSize(sources, window.innerWidth);
+
+    if (prevState.image !== null && imgToLoad !== prevState.image) {
+      this.setState({ image: placeholder, loading: true }, () => {
+        this.loadImage(imgToLoad);
+      });
+    }
+  }
+
+  /**
+   * Return the optimal image source for the device type
+   *
+   * @param {Array} sources Array of objects with src path and size
+   * @param {Int} windowWidth Width of the browser window
+   */
+  getIdealSize(sources, windowWidth) {
+    const srcsBiggerThanWindow = sources.filter(src => src.size > windowWidth);
+    if (srcsBiggerThanWindow.length) {
+      return srcsBiggerThanWindow[0].src;
+    } else {
+      return sources[sources.length - 1].src;
+    }
+  }
+
+  /**
+   * Begin the loading of the image
+   *
+   * @param {String} srcPath Source path for the image to load
+   */
+  loadImage = srcPath => {
+    if (this.image) {
+      this.image.onload = null;
+      this.image.onerror = null;
+    }
+    const image = new Image();
+    this.image = image;
+    image.onload = this.onLoad;
+    image.onerror = this.onError;
+    image.src = srcPath;
+  };
+
+  /**
+   * Update the component state when image loading is completed
+   */
+  onLoad = () => {
+    this.setState({
+      image: this.image.src,
+      loading: false
+    });
+  };
+
+  /**
+   * Call prop function (if any) when an error occurs during image load
+   */
+  onError = errorEvent => {
+    const { onError } = this.props;
+    if (onError) {
+      onError(errorEvent);
+    }
+  };
+
+  /**
+   * Expose the image and loading state to the render prop
+   */
   render() {
-    return (
-      <img
-        src="https://source.unsplash.com/random/500x300"
-        alt="A picture is worth a thousand words"
-      />
-    );
+    const { image, loading } = this.state;
+    const { children } = this.props;
+    if (!children || typeof children !== 'function') {
+      throw new Error(`PowerPicture requires a function as its only child`);
+    }
+    return children(image, loading);
   }
 }
+
+export default PowerPicture;
